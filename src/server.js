e34eb9de8150111cbe1480b474209e1b151ca6be
Lacey Washington <lacey.m.washington@gmail.com>
2024-08-04 14:13:46 -0500

    added socket libary

--- src/server.js
+++ src/server.js
@@ -4,9 +4,15 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const AWS = require('aws-sdk');
+const http = require('http');
+const WebSocket = require('ws');
 
-// Esoteric Resources
-
+// AWS config
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
 
 // Prepare the express app
 const app = express();
@@ -16,20 +22,35 @@ app.use(cors());
 app.use(morgan('dev'));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-// AWS config
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
+app.get('/', (req,res) => res.send('this is the back end of Ptolemy'));
+
+const server = http.createServer(app);
+
+
+const wss = new WebSocket.Server({ server: server });
+
+wss.on('connection', function connection(ws) {
+  console.log('A new client has connected');
+  ws.send('Welcome to Ptolemy intrepid hero!');
+
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
 });
 
+
+
 module.exports = {
-    server:app,
-    startUp: (port) => {
-        app.listen(port, () => {
-            console.log(`Server is up and running on ${port}`)
-        });
-    },
+  server,
+  startUp: (port) => {
+    server.listen(port, () => {
+      console.log(`Server is up and running on ${port}`);
+    });
+  },
 };
